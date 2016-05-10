97cd6391e70f8058db7e4734569d4818818815ca
Taras Leskiv <taras@getsocial.im>
2016-05-10 11:45:50 +0200

    generating code

---
+++ unity-source/Assets/Scripts/Editor/BridgeGenerator/AndroidBridgeClassGenerator.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.CodeDom;
+using UnityEditor;
+using System.CodeDom.Compiler;
+using System.IO;
+
+namespace Editor.BridgeGenerator
+{
+    public class AndroidBridgeClassGenerator
+    {
+        private const string OutputFileName = "AndroidBridge.cs";
+
+        CodeCompileUnit targetUnit;
+        CodeTypeDeclaration targetClass;
+
+        [MenuItem("Generate/Android")]
+        public static void GenerateBeidge()
+        {
+            var bridge = new AndroidBridgeClassGenerator();
+            var path = Application.dataPath + "/Generated/" + OutputFileName;
+            Debug.Log(path);
+            bridge.GenerateCSharpCode(path);
+            AssetDatabase.Refresh();
+        }
+
+        public AndroidBridgeClassGenerator()
+        {
+            targetUnit = new CodeCompileUnit();
+            var codeNamespace = new CodeNamespace("GeneratedBridge");
+
+            targetClass = new CodeTypeDeclaration("AndroidBridge");
+            targetClass.IsClass = true;
+            targetClass.TypeAttributes = System.Reflection.TypeAttributes.Public | System.Reflection.TypeAttributes.Sealed;
+            codeNamespace.Types.Add(targetClass);
+            targetUnit.Namespaces.Add(codeNamespace);
+        }
+
+        public void GenerateCSharpCode(string fileName)
+        {
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+            var options = new CodeGeneratorOptions();
+            options.BracingStyle = "C";
+            using (StreamWriter sourceWriter = new StreamWriter(fileName))
+            {
+                provider.GenerateCodeFromCompileUnit(targetUnit, sourceWriter, options);
+            }
+        }
+    }
+}
