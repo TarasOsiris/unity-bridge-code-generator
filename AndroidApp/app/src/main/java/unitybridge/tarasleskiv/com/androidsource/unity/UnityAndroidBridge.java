7e6fdb0a662c86494030d01e3c6b83bc4dd13d97
Taras Leskiv <taras@getsocial.im>
2016-05-13 10:46:17 +0200

    simple methods added

--- AndroidApp/app/src/main/java/unitybridge/tarasleskiv/com/androidsource/unity/UnityAndroidBridge.java
+++ AndroidApp/app/src/main/java/unitybridge/tarasleskiv/com/androidsource/unity/UnityAndroidBridge.java
@@ -6,12 +6,19 @@ package unitybridge.tarasleskiv.com.androidsource.unity;
 
 import android.util.Log;
 
-public class UnityAndroidBridge {
+public final class UnityAndroidBridge {
 	private static final String TAG = UnityAndroidBridge.class.getSimpleName();
+
+	public static final String MOCK = "STRING_VALUE";
 
 	public UnityAndroidBridge() {
 		Log.d(TAG, "UnityAndroidBridge constructor without params");
 	}
 
+	public static void setStringValue(String strValue) {
+	}
 
+	public static String getStringValue() {
+		return MOCK;
+	}
 }
